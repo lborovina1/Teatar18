b385edca82f82fce3cdd333fc0efccc030cee3bd
Merjem <mgutosic1@etf.unsa.ba>
2024-05-24 23:34:19 +0200

    Dodane modelske klase Predstava i Izvedba

--- Teatar18_2/Teatar18_2/Models/Predstava.cs
+++ Teatar18_2/Teatar18_2/Models/Predstava.cs
@@ -10,14 +10,10 @@ namespace Teatar18_2.Models
 
         [Key] public int ID { get; set; }
         public String Naziv { get; set; }
-
         public List<String> glumci { get; set; }
-
         public List<String> scenristi { get; set; }
-
         public List<String> reziseri { get; set; }
         public List<String> scenografi { get; set; }
-
         public Zanr zanr { get; set; }
         public String opis { get; set; }
         public Uri poster { get; set; }
@@ -26,13 +22,7 @@ namespace Teatar18_2.Models
         public List<int> slobodneKarte { get; set; }
         public bool uRepertoaru { get; set; }
 
-        public void ocijeniPredstavu()
-        {
-        }
-        public double izracunajProsjecnuOcjenu()
-        {
-            return ocjene.Average();
-           
-        }
+        public void ocijeniPredstavu(){ }
+        public double izracunajProsjecnuOcjenu(){ return ocjene.Average(); }
     }
 }
