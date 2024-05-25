e53a0c97a595e8ba4eea509278d89ce73dc490a3
Ivona <ijozic1@etf.unsa.ba>
2024-05-25 02:25:53 +0200

    Dodane modelske klase Karta i Ocjena

---
+++ Teatar18_2/Teatar18_2/Models/Karta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Karta
+{
+	public Karta(){}
+    [Key] public int ID { get; set; }
+    [ForeignKey("Izvedba")] public int IDIzvedbe { get; set; }
+    public int sjediste { get; set; }
+    public double cijena { get; set; }
+    public bool placena { get; set; } = false;
+    [ForeignKey("Rezervacija")] public int IDRezervacije { get; set; }  
+
+}
